7e72dd6dde035b8a23088829be477ba561ebc3e2
quentin.perret <quentinprrt33@gmail.com>
2022-10-20 11:33:30 +0200

    ukvewr

--- program.m
+++ program.m
@@ -9,5 +9,17 @@ imshow(frame)
 x2=fix(x); 
 y2=fix(y);
 Xvid  = [x2';y2'];
+%Récupération des points de l'image à intégrer
+i=imread("meme.jpg");
+taille_im=size(i);
+taille_frame  = size(frame);
+x1=[0,0,taille_im(2),taille_im(2)];
+y1=[0,taille_im(1),taille_im(1),0];
+Xim  = [x1; y1];
 
-remplacement(frame,Xvid);
+%Définition de la matrice d'homographie
+H=defH(Xvid,Xim);
+
+
+
+remplacement(frame,Xvid,taille_im,taille_frame,H);
--- remplacement.m
+++ remplacement.m
@@ -1,15 +1,5 @@
-function [img] = remplacement(frame,Xvid)
+function [img] = remplacement(frame,Xvid,taille_im,taille_frame,H)
     
-    %Récupération des points de l'image à intégrer
-    i=imread("meme.jpg");
-    taille_im=size(i);
-    taille_frame  = size(frame);
-    x1=[0,0,taille_im(2),taille_im(2)];
-    y1=[0,taille_im(1),taille_im(1),0];
-    Xim  = [x1; y1];
-    
-    %Définition de la matrice d'homographie
-    H=defH(Xvid,Xim);
     
     %Réalisation de l'homographie
 
@@ -21,4 +11,10 @@ function [img] = remplacement(frame,Xvid)
     %calcul des coordonnées de sortie à l'aide de H
     NewPosHom = H*position;
     NewPos = [round(NewPosHom(1,:) ./ NewPosHom(3,:)) ; round(NewPosHom(2,:) ./ NewPosHom(3,:))];
+
+    %on récupère les indices correspond à la zone de la feuille
+    Fzone = (NewPos(1,:)>0).*(NewPos(1,:)<taille_im(2)).*(NewPos(2,:)>0).*(NewPos(2,:)<taille_frame(1));
+
+    NewX = NewPos();
+
 end
