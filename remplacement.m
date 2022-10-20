d0b0496139a977bb92df2c2aec7707521de0202b
quentin.perret <quentinprrt33@gmail.com>
2022-10-20 11:04:15 +0200

    new position created by homography

--- remplacement.m
+++ remplacement.m
@@ -1,17 +1,24 @@
-function [img] = remplacement(Xvid)
+function [img] = remplacement(frame,Xvid)
     
     %Récupération des points de l'image à intégrer
     i=imread("meme.jpg");
-    taille_im=size(i,1:2);
+    taille_im=size(i);
+    taille_frame  = size(frame);
     x1=[0,0,taille_im(2),taille_im(2)];
     y1=[0,taille_im(1),taille_im(1),0];
-    Xim  = [[x1;]; [y1;];];
+    Xim  = [x1; y1];
     
     %Définition de la matrice d'homographie
-    new_H=defH(Xvid,Xim);
+    H=defH(Xvid,Xim);
     
     %Réalisation de l'homographie
-    taille_v = size(img);
-    truc=calculetousPoints(taille_v,new_H);
 
+    %creation d'une matrice position regroupant l'ensemble des positions
+    %des pixels de la frame
+    [x2,y2] = meshgrid(1:taille_frame(2),1:taille_frame(1));
+    position = [x2(:)';y2(:)';x2(:)'*0+1];
+    
+    %calcul des coordonnées de sortie à l'aide de H
+    NewPosHom = H*position;
+    NewPos = [round(NewPosHom(1,:) ./ NewPosHom(3,:)) ; round(NewPosHom(2,:) ./ NewPosHom(3,:))];
 end
