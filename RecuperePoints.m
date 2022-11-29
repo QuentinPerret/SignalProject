24baa70fb29b6ffd39069461d3fcd1052c53a08e
Hugo Tavard <htavard@bordeaux-inp.fr>
2022-11-29 11:17:32 +0100

    com RecuperePoints

--- RecuperePoints.m
+++ RecuperePoints.m
@@ -1,7 +1,11 @@
 function [pointsARelier] = RecuperePoints(x,y,z,taille)
-%RECUPEREPOINTS Summary of this function goes here
-%   Detailed explanation goes here
+%--- Classe les points de coordonnées x,y,z dans l'ordre prévu pour tracer
+%--- les segments
+
+% Initialisation des points à relier
 pointsARelier=[x(1,1)*taille(2) y(1,1)*taille(1) z(1,1); x(1+1,1)*taille(2) y(1+1,1)*taille(1) z(1+1,1);x(1,1)*taille(2) y(1,1)*taille(1) z(1,1);x(1,1+1)*taille(2) y(1,1+1)*taille(1) z(1,1+1)];
+
+% Boucle pour mettre deux points à relier a la suite dans la liste
 for i=1:size(x,1)-1
     for j=1:size(x,2)-1
         pointsARelier=[pointsARelier;x(i,j)*taille(2) y(i,j)*taille(1) z(i,j); x(i+1,j)*taille(2) y(i+1,j)*taille(1) z(i+1,j)];
@@ -9,5 +13,7 @@ for i=1:size(x,1)-1
     end
 end
 
+% On laisse volontairement la dernière colonne non reliée pour faire la
+% porte de l'igloo
 end
 
