73017257d95650f1f7b820c8f005b613092da6f7
Croquignoles <htavard@ensc.fr>
2022-11-29 15:05:07 +0100

    final fct name

--- RemplaceVerif3D.m
+++ RemplaceVerif3D.m
@@ -1,4 +1,4 @@
-function [frame] = RemplaceVerif3D(frame,pointsARelier,P)
+function [frame] = RemplaceVerif3D(frame,pointsARelier,P,writerObj,taille)
     
     %dimVid = size(frame);
     
@@ -11,20 +11,14 @@ function [frame] = RemplaceVerif3D(frame,pointsARelier,P)
         x2=round(X2(1)/z);
         y2=round(X2(2)/z);
         if x2>0 && x2<size(frame,2) && y2>0 && y2<size(frame,1)
-            pts2D(i,1)=x2;
-            pts2D(i,2)=y2;
+            pts2D(i,1)=x2-taille(1)/2;
+            pts2D(i,2)=y2+3*taille(2)/5;
         end
     end
-
+    frame=Trace(frame,pts2D);
     imshow (uint8(frame));
-    hold on;
-    % Affichage des coins
-    for i = 1:size(pts2D,1)   
-        plot(pts2D(i,1) , pts2D(i,2), 'r+', 'MarkerSize', 2, 'LineWidth', 2);    
-    end
-    hold off;
     % Ajout de l'image sur la vidéo
     frameNew = getframe(gcf) ;
-    writeVideo(videoAvecCoins, frameNew);
+    writeVideo(writerObj, frameNew);
 end
 
