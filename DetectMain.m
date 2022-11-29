f12063ed50651631ea3a0422920b87b45af2027c
Hugo Tavard <htavard@bordeaux-inp.fr>
2022-11-29 10:44:06 +0100

    Com DetectMain

--- DetectMain.m
+++ DetectMain.m
@@ -1,8 +1,8 @@
-function [M] = detectionMain(frame)
+function [M] = DetectMain(frame)
+%--- Détecte la main grâce à un seuil 
+
     R=frame(:,:,1);
-    G=frame(:,:,2);
-    B=frame(:,:,3);
-    Y=0.299*R+0.587*G+0.114*B;
+    Y=CouleurToGris(frame);
     Cr=0.713*(R-Y)+128;
     M=abs(double(Cr>129)-1);
 end
