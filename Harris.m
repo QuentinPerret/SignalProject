45514f11016792a07ddb32c440205139f139810f
Hugo Tavard <htavard@bordeaux-inp.fr>
2022-11-29 10:45:53 +0100

    com Harris

--- Harris.m
+++ Harris.m
@@ -1,10 +1,18 @@
 function [detecteur]=Harris(sigma,sigmaG,image)
-[Ix,Iy]=Canny(image,sigmaG);
+%--- Renvoie le détecteur de Harris en tout point
+
+[Ix,Iy]=Canny(image,sigmaG); %Gradient en x et y
 [X,Y]=meshgrid(-ceil(sigma*3):ceil(sigma*3));
+
+% Gaussienne
 G_sigmaC=1/(2*pi*sigma^2)*exp(-(X.*X+Y.*Y)/(2*sigma*sigma));
+
+% Covariances
 Cxx=conv2(Ix.*Ix,G_sigmaC,'same');
 Cyy=conv2(Iy.*Iy,G_sigmaC,'same');
 Cxy=conv2(Ix.*Iy,G_sigmaC,'same');
+
+% Détecteur
 det=Cxx.*Cyy-(Cxy.*Cxy);
 Tr=Cxx+Cyy;
 k=0.05;
