6009c31369c81c28456cc9a1514c1d82ae88a9e8
Hugo Tavard <htavard@bordeaux-inp.fr>
2022-11-29 10:41:00 +0100

    Com DetectCoin

--- DetectCoin.m
+++ DetectCoin.m
@@ -1,5 +1,7 @@
 function coins=DetectCoin(detecteur, coinsImage1,coinsImage2)
-coins=coinsImage2;
+%--- Détecte la position des coins 
+
+coins=coinsImage2; %Les coins sont initalisés pour être les mêmes que l'image précédente 
 for i=1:size(coins,1)
     coins(i,:)=ValMaxHarris(detecteur,coinsImage1(i,:),coinsImage2(i,:));
 end
