2ef7db05021e5db726839816cbfc363a3d7d2b0e
Croquignoles <htavard@ensc.fr>
2022-11-29 10:21:32 +0100

    com CouleurToGris

--- Canny.m
+++ Canny.m
@@ -1,5 +1,6 @@
 function [Ix,Iy]=Canny(image,sigma) 
 %--- Fonction qui renvoie les composantes x et y du gradient
+
 [X,Y]=meshgrid(-ceil(sigma*3):ceil(sigma*3)); %On construit la grille pour de façon à avoir 99,7% de l'information
 Hx=-X/(2*pi*sigma.^4).*exp(-(X.^2+Y.^2)/(2*sigma^2)); %dérivée de la gaussienne selon x
 Hy=-Y/(2*pi*sigma.^4).*exp(-(X.^2+Y.^2)/(2*sigma^2)); %dérivée de la gaussienne selon y
--- CouleurToGris.m
+++ CouleurToGris.m
@@ -1,5 +1,8 @@
 function I = CouleurToGris(image)
-R=image(:,:,1);
-G=image(:,:,2);
-B=image(:,:,3);
+%--- Renvoie une image en niveau de gris 
+
+R=image(:,:,1); % Niveau de rouge en tout pixel 
+G=image(:,:,2); % Niveau de vert 
+B=image(:,:,3); % Niveau de bleu 
+% Luminance perçue
 I=0.299*R+0.587*G+0.114*B;
