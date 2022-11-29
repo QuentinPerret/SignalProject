f978a771e82c64fa2d29b4b4aaddea51a15064b9
Hugo Tavard <htavard@bordeaux-inp.fr>
2022-11-29 10:46:32 +0100

    com HarrisMultiEchelle

--- HarrisMultiEchelle.m
+++ HarrisMultiEchelle.m
@@ -1,4 +1,6 @@
 function [detecteurMulti]=HarrisMultiEchelle(sigma1,sigma2,sigmaG,image)
+%--- Renvoie le détecteur de harris le plus adapté au traitement de l'image
+
 detecteur1=Harris(sigma1,sigmaG,image);
 detecteur2=Harris(sigma2,sigmaG,image);
 detecteurMulti=min(detecteur1.*abs(detecteur2),abs(detecteur1).*detecteur2);
